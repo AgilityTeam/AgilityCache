0891895b3f86494801573c777e7ca03833f7e76d
Joaquim Pedro França Simão (osmano807) <osmano807@gmail.com>
2012-02-09 11:01:24 -0200

    Removing trailing white space

--- apps/agilitycache/src/agilitycache_cache_plugin.erl
+++ apps/agilitycache/src/agilitycache_cache_plugin.erl
@@ -7,13 +7,13 @@ behaviour_info(callbacks) ->
 		%% -spec name() -> binary().
 		{name,0},
 		%% -spec in_charge(#http_req{}) -> boolean().
-		{in_charge, 1}, 
+		{in_charge, 1},
 		%% -spec cacheable(#http_req{}, #http_rep{}) -> boolean().
-		{cacheable, 2}, 
+		{cacheable, 2},
 		%% -spec file_id(#http_req{}) -> binary(). [md5sum]
-		{file_id, 1},		
+		{file_id, 1},
 		%% -spec expires(calendar:datetime(), #http_req{}, #http_rep{}) -> calendar:datetime()
-		{expires, 3}		
+		{expires, 3}
 	];
 behaviour_info(_Other) ->
     undefined.
