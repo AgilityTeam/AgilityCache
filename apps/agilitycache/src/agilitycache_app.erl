91403d82f6b5acb087445fffacd7ec8d303d46bb
Joaquim Pedro França Simão (osmano807) <osmano807@gmail.com>
2012-02-16 14:34:08 -0200

    Add standalone start functions

--- apps/agilitycache/src/agilitycache_app.erl
+++ apps/agilitycache/src/agilitycache_app.erl
@@ -3,11 +3,23 @@
 -behaviour(application).
 
 %% Application callbacks
--export([start/2, stop/1]).
+-export([start/0, start/1, start/2, stop/1]).
 
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
+
+start() ->
+    start(agilitycache).
+
+start(App) ->
+    case application:start(App) of
+        {error, {not_started, Dep}} ->
+            start(Dep),
+            start(App);
+        Other ->
+            Other
+    end.
 
 start(_StartType, _StartArgs) ->
     agilitycache_sup:start_link().
