c1bbb0266b91e357f5c38d0ce5286ef2b3fc4da8
Joaquim Pedro França Simão (osmano807) <osmano807@gmail.com>
2012-06-08 10:54:43 -0300

    Adding imgur plugin

---
+++ apps/agilitycache_plugins/src/agilitycache_cache_plugin_imgur.erl
@@ -0,0 +1,51 @@
+-module(agilitycache_cache_plugin_imgur).
+-behaviour(agilitycache_cache_plugin).
+
+%% vimerl compile fix
+-ifdef(VIMERL).
+-include("../agilitycache/include/http.hrl").
+-else.
+-include_lib("agilitycache/include/http.hrl").
+-endif.
+
+-export([
+         name/0,
+         in_charge/1,
+         cacheable/1,
+         cacheable/2,
+         file_id/1,
+         expires/2
+        ]).
+
+-spec name() -> binary().
+name() ->
+	<<"Imgur">>.
+
+-spec in_charge(#http_req{}) -> boolean().
+in_charge(_HttpReq = #http_req{ uri=_Uri=#http_uri{domain = <<"i.imgur.com">>, port = 80 }}) ->
+	true;
+in_charge(_) ->
+	false.
+
+-spec cacheable(#http_req{}) -> boolean().
+cacheable(_HttpReq) ->
+	true.
+
+-spec cacheable(#http_req{}, #http_rep{}) -> boolean().
+cacheable(_HttpReq, _HttpRep) ->
+	true.
+
+-spec file_id(#http_req{}) -> binary().
+file_id(_HttpReq = #http_req{ uri=_Uri=#http_uri{path = RawPath, port = Port }}) ->
+	%%"http://Plugin." + name() + "/" + request.cannonical_url;
+	Name = name(),
+	BinaryPort = list_to_binary(integer_to_list(Port)),
+	File = filename:rootname(RawPath), % remove extensão
+	%% XXX: Put raw_qs?
+	B = <<"http://Plugin.", Name/binary, ":", BinaryPort/binary, File/binary>>,
+	lager:debug("B: ~p", [B]),
+	erlang:md5(B).
+
+-spec expires(#http_req{}, #http_rep{}) -> calendar:datetime().
+expires(_HttpReq, HttpRep) ->
+	agilitycache_cache_plugin_utils:parse_max_age(HttpRep).
